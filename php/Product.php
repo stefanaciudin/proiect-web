5a2f7d4f2a18119a37942baacc07d2bd7813edb1
stefanaciudin <stefanaciudin@gmail.com>
2023-06-22 22:13:36 +0300

    final touches - comms and deleting unused files

--- php/Product.php
+++ php/Product.php
@@ -18,27 +18,11 @@ class Product
     protected int $times_recommended;
 
     /**
-     * @return int
-     */
-    public function getProductId(): int
-    {
-        return $this->product_id;
-    }
-
-    /**
      * @param int $product_id
      */
     public function setProductId(int $product_id): void
     {
         $this->product_id = $product_id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
     }
 
     /**
@@ -50,14 +34,6 @@ class Product
     }
 
     /**
-     * @return float
-     */
-    public function getPrice(): float
-    {
-        return $this->price;
-    }
-
-    /**
      * @param float $price
      */
     public function setPrice(float $price): void
@@ -65,13 +41,6 @@ class Product
         $this->price = $price;
     }
 
-    /**
-     * @return string
-     */
-    public function getImagePath(): string
-    {
-        return $this->image_path;
-    }
 
     /**
      * @param string $image_path
@@ -82,14 +51,6 @@ class Product
     }
 
     /**
-     * @return int
-     */
-    public function getIsMakeup(): int
-    {
-        return $this->is_makeup;
-    }
-
-    /**
      * @param int $is_makeup
      */
     public function setIsMakeup(int $is_makeup): void
@@ -97,28 +58,9 @@ class Product
         $this->is_makeup = $is_makeup;
     }
 
-    /**
-     * @return int
-     */
-    public function getAge(): int
-    {
-        return $this->age;
-    }
-
-    /**
-     * @param int $age
-     */
     public function setAge(int $age): void
     {
         $this->age = $age;
-    }
-
-    /**
-     * @return int
-     */
-    public function getBrandId(): int
-    {
-        return $this->brand_id;
     }
 
     /**
@@ -129,13 +71,6 @@ class Product
         $this->brand_id = $brand_id;
     }
 
-    /**
-     * @return int
-     */
-    public function getSkintypeId(): int
-    {
-        return $this->skintype_id;
-    }
 
     /**
      * @param int $skintype_id
@@ -143,14 +78,6 @@ class Product
     public function setSkintypeId(int $skintype_id): void
     {
         $this->skintype_id = $skintype_id;
-    }
-
-    /**
-     * @return int
-     */
-    public function getTypeId(): int
-    {
-        return $this->type_id;
     }
 
     /**
@@ -162,27 +89,11 @@ class Product
     }
 
     /**
-     * @return string
-     */
-    public function getIngredients(): mixed
-    {
-        return $this->ingredients;
-    }
-
-    /**
      * @param string $ingredients
      */
     public function setIngredients(mixed $ingredients): void
     {
         $this->ingredients = $ingredients;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription(): mixed
-    {
-        return $this->description;
     }
 
     /**
@@ -194,14 +105,6 @@ class Product
     }
 
     /**
-     * @return string
-     */
-    public function getHowToUse(): mixed
-    {
-        return $this->how_to_use;
-    }
-
-    /**
      * @param string $how_to_use
      */
     public function setHowToUse(mixed $how_to_use): void
@@ -209,13 +112,6 @@ class Product
         $this->how_to_use = $how_to_use;
     }
 
-    /**
-     * @return string
-     */
-    public function getLink(): string
-    {
-        return $this->link;
-    }
 
     /**
      * @param string $link
@@ -223,14 +119,6 @@ class Product
     public function setLink(string $link): void
     {
         $this->link = $link;
-    }
-
-    /**
-     * @return int
-     */
-    public function getTimesRecommended(): int
-    {
-        return $this->times_recommended;
     }
 
     /**
