12e0ac9cbd02cfa8becac45d0842edbdfd904ecb
Ionela Alexandra <63660313+ionero12@users.noreply.github.com>
2023-06-22 15:11:06 +0300

    bia branch

--- makeup.php
+++ makeup.php
@@ -4,6 +4,8 @@ include "php/VideoRepository.php";
 $product = new ProductRepository();
 $video = new VideoRepository();
 $products = [];
+$products = $product->getProductsByBrands();
+
 
 if (isset($_POST['submit'])) {
     $products = $product->getProductsBySort();
@@ -69,28 +71,15 @@ if (isset($_POST['submit_sort'])) {
             <button class="button" id="filter">Filtreaza</button>
             <button class="button" id="sort">Sorteaza</button>
         </div>
-
         <div id="carousel" class="carousel">
             <button id="prevBtn" class="carousel-btn prev-btn">&lt;</button>
             <div id="videosContainer" class="videos-container">
-                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/sDErfOvnUfg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <button id="nextBtn" class="carousel-btn next-btn">&gt;</button>
         </div>
@@ -98,23 +87,31 @@ if (isset($_POST['submit_sort'])) {
         <!--        --><?php
         //        $occasion = "mireasa";
         //        $videos = $video->getVideosByOccasion($occasion);
-        //        ?><!-- --><?php
+        //
+        ?><!-- --><?php
         //        $occasion = "mireasa";
         //        $videos = $video->getVideosByOccasion($occasion);
-        //        ?>
+        //
+        ?>
         <!---->
         <!--        <div id="carousel" class="carousel">-->
         <!--            <button id="prevBtn" class="carousel-btn prev-btn">&lt;</button>-->
         <!--            <div id="videosContainer" class="videos-container">-->
-        <!--                --><?php //if (empty($videos)): ?>
+        <!--                --><?php //if (empty($videos)):
+        ?>
         <!--                    <p>No videos available.</p>-->
-        <!--                --><?php //else: ?>
-        <!--                    --><?php //foreach ($videos as $video): ?>
-        <!--                        <iframe src="--><?php //echo $video['link']; ?><!--" title="YouTube video player"-->
+        <!--                --><?php //else:
+        ?>
+        <!--                    --><?php //foreach ($videos as $video):
+        ?>
+        <!--                        <iframe src="--><?php //echo $video['link'];
+        ?><!--" title="YouTube video player"-->
         <!--                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"-->
         <!--                                allowfullscreen></iframe>-->
-        <!--                    --><?php //endforeach; ?>
-        <!--                --><?php //endif; ?>
+        <!--                    --><?php //endforeach;
+        ?>
+        <!--                --><?php //endif;
+        ?>
         <!--            </div>-->
         <!--            <button id="nextBtn" class="carousel-btn next-btn">&gt;</button>-->
         <!--        </div>-->
@@ -122,7 +119,8 @@ if (isset($_POST['submit_sort'])) {
         <!--        --><?php
         //               $occasion = "mireasa";
         //                $videos = $video->getVideosByOccasion($occasion);
-        //                ?>
+        //
+        ?>
         <!--        <script>-->
         <!--            function createVideoElement(videos) {-->
         <!--                const videosContainer = document.getElementById('videosContainer');-->
@@ -147,23 +145,32 @@ if (isset($_POST['submit_sort'])) {
         <!---->
         <!--        </script>-->
 
-
         <div class="products_phone" id="products_phone">
             <?php
             if ($products != []) {
                 foreach ($products as $prod) {
-                    echo '<div class="product">';
-                    echo '<a href="' . $prod['link'] . '">';
-                    echo '<img src="' . $prod['image_path'] . '" alt="' . $prod['description'] . '">';
-                    echo '<p>' . $prod['name'] . ' - ' . $prod['price'] . ' lei' . '</p>';
-                    echo '</a>';
-                    echo '</div>';
+                    if(isset($prod['name'])){
+                        echo '<div class="product">';
+                        echo '<a href="' . $prod['link'] . '">';
+                        echo '<img src="' . $prod['image_path'] . '" alt="' . $prod['description'] . '">';
+                        echo '<p>' . $prod['name'] . ' - ' . $prod['price'] . ' lei' . '</p>';
+                        echo '</a>';
+                        echo '</div>';
+                    }elseif(isset($prod['occasion'])){
+                        echo '<div class="product">';
+                        echo '<iframe src="'.$prod['link'].'" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+                        echo '</div>';
+                    }
+
                 }
             } else {
                 echo "<p>Nu exista produse pentru filtrele selectate!</p>";
             }
             ?>
         </div>
+        <!-- <div class="video_mobile">
+
+        </div> -->
 
         <div id="products" class="desktop_only">
             <!-- Products will be populated dynamically -->
@@ -178,6 +185,10 @@ if (isset($_POST['submit_sort'])) {
             <p></p>
 
             <form class="form-container" method="POST" action="makeup.php">
+                <div class="checkbox-container">
+                    <input type="checkbox" id="checkbox4" name="checkboxGroup" value="checkbox4" onchange="toggleDiv('div4', this)">
+                    <label for="checkbox4">Selecteaza tipul de tutorial de machiaj:</label>
+                </div>
                 <select id="s1" name="select1" class="select_brands">
                     <?php
                     $brands = $product->getMakeUpBrands();
@@ -189,19 +200,24 @@ if (isset($_POST['submit_sort'])) {
                     ?>
                 </select>
                 <div class="checkbox-container">
-                    <input type="checkbox" id="checkbox1" name="checkboxGroup" value="checkbox1"
-                           onchange="toggleDiv('div1', this)">
+                    <input type="checkbox" id="checkbox1" name="checkboxGroup" value="checkbox1" onchange="toggleDiv('div1', this)">
                     <label for="checkbox1">Ten</label>
                 </div>
                 <div class="checkbox-container">
-                    <input type="checkbox" id="checkbox2" name="checkboxGroup" value="checkbox2"
-                           onchange="toggleDiv('div2', this)">
+                    <input type="checkbox" id="checkbox2" name="checkboxGroup" value="checkbox2" onchange="toggleDiv('div2', this)">
                     <label for="checkbox2">Ochi</label>
                 </div>
                 <div class="checkbox-container">
-                    <input type="checkbox" id="checkbox3" name="checkboxGroup" value="checkbox3"
-                           onchange="toggleDiv('div3', this)">
+                    <input type="checkbox" id="checkbox3" name="checkboxGroup" value="checkbox3" onchange="toggleDiv('div3', this)">
                     <label for="checkbox3">Buze</label>
+                </div>
+                <div id="div4" class="hidden">
+                    <select id="select5" name="select5" class="select_video">
+                        <option value="zi">Machiaj de zi</option>
+                        <option value="evenimet">Machiaj de eveniment</option>
+                        <option value="mireasa">Machiaj de mireasa</option>
+                        <option value="seara">Machiaj de seara</option>
+                    </select>
                 </div>
                 <div id="div1" class="hidden">
 
@@ -305,24 +321,24 @@ if (isset($_POST['submit_sort'])) {
     }
 
     // Event listener for opening the filter modal
-    filterBtn.onclick = function () {
+    filterBtn.onclick = function() {
         openModal(filterModal);
     };
 
     // Event listener for opening the sort modal
-    sortBtn.onclick = function () {
+    sortBtn.onclick = function() {
         openModal(sortModal);
     };
 
     // Event listener for closing the modals
     for (var i = 0; i < closeBtns.length; i++) {
-        closeBtns[i].onclick = function () {
+        closeBtns[i].onclick = function() {
             closeModal(this.parentElement.parentElement);
         };
     }
 
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
+    window.onclick = function(event) {
         if (event.target == filterModal) {
             closeModal(filterModal);
         }
@@ -430,17 +446,17 @@ if (isset($_POST['submit_sort'])) {
         });
 
     // Event listener for brand change
-    document.getElementById('brand').addEventListener('change', function (event) {
+    document.getElementById('brand').addEventListener('change', function(event) {
         handleSelectChange(event, 'http://127.0.0.1:8000/api/find-products-by-brand-and-category.php?product_category=1&brand_name=', createProductElements);
     });
 
     // Event listener for product type change
-    document.getElementById('product_type').addEventListener('change', function (event) {
+    document.getElementById('product_type').addEventListener('change', function(event) {
         handleSelectChange(event, 'http://127.0.0.1:8000/api/find-makeup-products-by-type.php?type=', createProductElements);
     });
 
     // Event listener for usage type change
-    document.getElementById('usage_type').addEventListener('change', function (event) {
+    document.getElementById('usage_type').addEventListener('change', function(event) {
         handleSelectChange(event, 'http://127.0.0.1:8000/api/find-products-by-usage-type.php?product_category=1&usage_type=', createProductElements);
     });
 
@@ -481,7 +497,6 @@ if (isset($_POST['submit_sort'])) {
         }
         videosContainer.style.transform = `translateX(${currentPosition}px)`;
     });
-
 </script>
 
 </body>
